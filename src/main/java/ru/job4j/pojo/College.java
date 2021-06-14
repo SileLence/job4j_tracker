89aeccec68ea6ead922606e7fa30c8ea67d78296
SileLence <smkslnc@gmail.com>
2021-06-15 00:06:30 +0300

    2. Модель данных. [#174683]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -5,7 +5,7 @@ public class College {
         Student student = new Student();
         student.setFullName("Галактионов Арсений Матвеевич");
         student.setGroup("211-361");
-        student.setEnrollmentDate("2021-08.15");
+        student.setEnrollmentDate("2021-08-15");
         System.out.println("Имя: " + student.getFullName()
                 + System.lineSeparator()
                 + "Учебная группа: " + student.getGroup()
