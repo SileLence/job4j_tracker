ba5bbccec0fa3a4eeb0ea1b048c9a6ba65ffbf2d
SileLence <smkslnc@gmail.com>
2021-06-14 22:04:03 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class JsonReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                + "  " + "\"name\"" + ": " + "\"" + name + "\""
+                + "," + System.lineSeparator()
+                + "  " + "\"body\"" + ": " + "\"" + body + "\""
+                + System.lineSeparator() + "}";
+    }
+}
