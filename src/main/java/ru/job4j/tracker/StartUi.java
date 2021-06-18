0f932ea2909e500505251937ed2d6f9dc46d3f0f
SileLence <smkslnc@gmail.com>
2021-06-18 22:11:40 +0300

    9. toString [#310018 #168813]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -4,6 +4,6 @@ public class StartUi {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         Item item = new Item("ItemName");
-        System.out.println(item.toString());
+        System.out.println(item);
     }
 }
