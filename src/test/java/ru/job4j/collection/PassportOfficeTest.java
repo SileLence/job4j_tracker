9ae86cebcbe4653c45e3dbace31bfb8f0e53f00a
SileLence <smkslnc@gmail.com>
2021-08-14 00:39:08 +0300

    1. Паспорт и жители [#212656 #189254]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -8,10 +8,19 @@ import static org.junit.Assert.*;
 public class PassportOfficeTest {
 
     @Test
-    public void add() {
+    public void whenAdd() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenNoSameObjects() {
+        PassportOffice office = new PassportOffice();
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
     }
 }
