78453243e530ba24e6375e6f799b403f9f57aae3
SileLence <smkslnc@gmail.com>
2021-08-14 00:50:40 +0300

    2. Преобразования List в Map. [#10093 #189261]

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
