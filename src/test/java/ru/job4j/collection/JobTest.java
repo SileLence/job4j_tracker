e31aef66a6dec1a974fc10975bb2f82c516f2951
SileLence <smkslnc@gmail.com>
2021-08-18 01:48:59 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,83 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void whenAscByName() {
+        Comparator<Job> jobComparator = new JobAscByName();
+        int result = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void whenDescByName() {
+        Comparator<Job> jobComparator = new JobDescByName();
+        int result = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertTrue(result > 0);
+    }
+
+    @Test
+    public void whenAscByPriority() {
+        Comparator<Job> jobComparator = new JobAscByPriority();
+        int result = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertTrue(result > 0);
+    }
+
+    @Test
+    public void whenDescByPriority() {
+        Comparator<Job> jobComparator = new JobDescByPriority();
+        int result = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void whenAscByNameAndPriority() {
+        Comparator<Job> jobComparator =
+                new JobAscByName().thenComparing(new JobAscByPriority());
+        int differentNames = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertThat(differentNames, lessThan(0));
+        int sameNames = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Active", 1)
+        );
+        assertThat(sameNames, greaterThan(0));
+    }
+
+    @Test
+    public void whenDescByNameAndPriority() {
+        Comparator<Job> jobComparator =
+                new JobDescByName().thenComparing(new JobDescByPriority());
+        int differentNames = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Direct", 1)
+        );
+        assertThat(differentNames, greaterThan(0));
+        int sameNames = jobComparator.compare(
+                new Job("Active", 2),
+                new Job("Active", 1)
+        );
+        assertThat(sameNames, lessThan(0));
+    }
+}
