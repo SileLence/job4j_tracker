6c951e4d511a89be483ed365f4689ed01a11a67a
SileLence <smkslnc@gmail.com>
2021-09-20 23:24:58 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,17 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String str1 = "left";
+        String str2 = "right";
+        Comparator<String> stringComparator = (left, right) -> {
+            System.out.println("Compare strings by length descending — " + left.length()
+                    + " : " + right.length());
+            System.out.print("Result: ");
+            return right.length() - left.length();
+        };
+        System.out.println(stringComparator.compare(str1, str2));
+    }
+}
