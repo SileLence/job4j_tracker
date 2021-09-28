104784169f6de92bc2ab7c7570d92151a2cc8057
SileLence <smkslnc@gmail.com>
2021-09-28 23:22:51 +0300

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(9, 2, 0, -2, 4, -7, -23, 12);
+        List<Integer> positive = numbers.stream()
+                .filter(num -> num > 0)
+                .collect(Collectors.toList());
+        System.out.println(positive);
+    }
+}
